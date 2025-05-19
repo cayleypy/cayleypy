11dc72e7e7dc0f600466ad6f8df17ae607d7c72d
Dmytro Fedoriaka <fedimser@gmail.com>
2025-05-19 08:16:17 -0700

    fix style

--- cayleypy/graphs_lib_test.py
+++ cayleypy/graphs_lib_test.py
@@ -5,6 +5,7 @@ def test_all_transpositions():
     graph = prepare_graph("all_transpositions", n=20)
     assert graph.n_generators == (20 * 19) // 2
 
+
 def test_cube333():
     graph = prepare_graph("cube_3/3/3_12gensQTM")
     assert graph.n_generators == 12
