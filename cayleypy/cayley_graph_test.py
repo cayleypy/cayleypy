b9be0c4163c74d8f720a9d26ac42d9a003502457
Dmytro Fedoriaka <fedimser@gmail.com>
2025-05-23 08:36:04 -0700

    fix test

--- cayleypy/cayley_graph_test.py
+++ cayleypy/cayley_graph_test.py
@@ -286,7 +286,7 @@ def test_cube333_htm():
 def test_all_transpositions():
     graph = prepare_graph("all_transpositions", n=10)
     result = graph.bfs()
-    assert result.layer_sizes == load_dataset("lrx_cayley_growth")["10"]
+    assert result.layer_sizes == load_dataset("all_transpositions")["10"]
 
 
 def test_generator_names():
