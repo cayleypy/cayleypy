b7aebd5e19fd59df1bed0ab2b822f1aedca34b4b
Apich238 <K,I2}z1^t[T?IA!Jq>
2025-06-10 20:31:59 +0300

    Добавил генераторы cubic pancake

--- cayleypy/graphs_lib_test.py
+++ cayleypy/graphs_lib_test.py
@@ -21,6 +21,79 @@ def test_pancake():
     ))
 
 
+def test_cubic_pancake():
+
+    graph = prepare_graph("cubic_pancake", n=15, S=1)
+    assert graph.n_generators == 3
+    assert graph.generator_names == ["R15", "R14", "R2"]
+    assert torch.equal(graph.generators, torch.tensor(
+        [
+            [14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0],
+            [13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 14],
+            [1, 0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+        ]
+    ))
+    graph = prepare_graph("cubic_pancake", n=15, S=2)
+    assert graph.n_generators == 3
+    assert graph.generator_names == ["R15", "R14", "R3"]
+    assert torch.equal(graph.generators, torch.tensor(
+        [
+            [14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0],
+            [13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 14],
+            [2, 1, 0, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+        ]
+    ))
+    graph = prepare_graph("cubic_pancake", n=15, S=3)
+    assert graph.n_generators == 3
+    assert graph.generator_names == ["R15", "R14", "R13"]
+    assert torch.equal(graph.generators, torch.tensor(
+        [
+            [14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0],
+            [13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 14],
+            [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 13, 14]
+        ]
+    ))
+    graph = prepare_graph("cubic_pancake", n=15, S=4)
+    assert graph.n_generators == 3
+    assert graph.generator_names == ["R15", "R14", "R12"]
+    assert torch.equal(graph.generators, torch.tensor(
+        [
+            [14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0],
+            [13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 14],
+            [11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 12, 13, 14]
+        ]
+    ))
+    graph = prepare_graph("cubic_pancake", n=15, S=5)
+    assert graph.n_generators == 3
+    assert graph.generator_names == ["R15", "R13", "R2"]
+    assert torch.equal(graph.generators, torch.tensor(
+        [
+            [14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0],
+            [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 13, 14],
+            [1, 0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+        ]
+    ))
+    graph = prepare_graph("cubic_pancake", n=15, S=6)
+    assert graph.n_generators == 3
+    assert graph.generator_names == ["R15", "R13", "R3"]
+    assert torch.equal(graph.generators, torch.tensor(
+        [
+            [14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0],
+            [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 13, 14],
+            [2, 1, 0, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+        ]
+    ))
+    graph = prepare_graph("cubic_pancake", n=15, S=7)
+    assert graph.n_generators == 3
+    assert graph.generator_names == ["R15", "R13", "R12"]
+    assert torch.equal(graph.generators, torch.tensor(
+        [
+            [14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0],
+            [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 13, 14],
+            [11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 12, 13, 14]
+        ]
+    ))
+    
 def test_full_reversals():
     graph = prepare_graph("full_reversals", n=4)
     assert graph.n_generators == 6
