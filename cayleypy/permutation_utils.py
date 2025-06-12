0b8f559af7dd9abc07c5e56ed4ff7287333af9b9
add-later <dariya.mamayeva28@gmail.com>
2025-06-12 04:44:05 +0000

    transposition_function added

--- cayleypy/permutation_utils.py
+++ cayleypy/permutation_utils.py
@@ -21,3 +21,24 @@ def inverse_permutation(p: Sequence[int]) -> list[int]:
 
 def is_permutation(p):
     return sorted(list(p)) == list(range(len(p)))
+
+
+def transpositions(n):
+    """
+    Returns all transpositions (single swaps) of n numbers (counting from 0).
+    Example:
+    transpositions(4) returns
+    [
+        [1, 0, 2, 3], [2, 1, 0, 3],
+        [3, 1, 2, 0], [0, 2, 1, 3],
+        [0, 3, 2, 1], [0, 1, 3, 2]
+    ]
+    """
+    result = []
+    for i in range(n):
+        for j in range(i+1, n):
+            p = list(range(n))
+            p[i], p[j] = p[j], p[i]
+            result.append(p)
+    assert len(result) == n * (n-1) // 2
+    return result
