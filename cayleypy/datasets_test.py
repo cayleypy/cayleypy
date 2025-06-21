8d6f49e026e81a09626822feb93a5e89bb9dd557
Anton <anton.naymov@gmail.com>
2025-06-21 18:27:01 +0300

    Fixed stuilng by disabling it where it didn't make sense

--- cayleypy/datasets_test.py
+++ cayleypy/datasets_test.py
@@ -111,8 +111,12 @@ def test_cyclic_coxeter_cayley_growth():
 
 
 def test_pyraminx_cayley_growth():
-    # https://github.com/rokicki/twsearch-results/blob/master/godsnumber/pyraminx-g.log
+    # fmt: off
+    # pylint: disable=line-too-long
+    # The layers size are the same as here: https://github.com/rokicki/twsearch-results/blob/master/godsnumber/pyraminx-g.log
     precomputed_layer_sizes = [1, 16, 136, 896, 5456, 32528, 190260, 1070699, 5421594, 21136359, 55716688, 91554722, 85678373, 37687176, 3822520, 13120, 336]
+    # fmt: on
+    # pylint: enable=line-too-long
     for key, layer_sizes in load_dataset("pyraminx_cayley_growth").items():
         print(key, layer_sizes)
         max_diam = int(key)
