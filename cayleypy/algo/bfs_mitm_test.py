1bbdd6b9e9f2ec9026746b3566e60579bb2e21a1
Dmytro Fedoriaka <fedimser@gmail.com>
2025-07-30 19:12:10 -0700

    add test

--- cayleypy/algo/bfs_mitm_test.py
+++ cayleypy/algo/bfs_mitm_test.py
@@ -9,11 +9,26 @@ def test_find_path_bfs_mitm_lrx10():
     start_state = [7, 9, 6, 1, 0, 8, 5, 3, 2, 4]
 
     # Too few layers, path not found.
-    result12 = find_path_bfs_mitm(graph, start_state, br12)
-    assert result12 is None
+    assert find_path_bfs_mitm(graph, start_state, br12) is None
 
     # To find path of length 26, need minimum of 13 layers in pre-computed BFS.
     path = find_path_bfs_mitm(graph, start_state, br13)
     assert path is not None
     assert len(path) == 26
+    graph.validate_path(start_state, path)
+
+
+def test_find_path_bfs_mitm_lrx20():
+    graph = CayleyGraph(PermutationGroups.lrx(20))
+    br11 = graph.bfs(max_diameter=11, return_all_hashes=True)
+    br12 = graph.bfs(max_diameter=12, return_all_hashes=True)
+    start_state = [10, 12, 13, 18, 14, 16, 15, 17, 19, 0, 1, 2, 3, 4, 5, 6, 7, 9, 8, 11]
+
+    # Too few layers, path not found.
+    assert find_path_bfs_mitm(graph, start_state, br11) is None
+
+    # To find path of length 24, need minimum of 12 layers in pre-computed BFS.
+    path = find_path_bfs_mitm(graph, start_state, br12)
+    assert path is not None
+    assert len(path) == 24
     graph.validate_path(start_state, path)
