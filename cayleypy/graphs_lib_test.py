fb294da9ef4eeffb7099379591962314abd9f61d
add-later <dariya.mamayeva28@gmail.com>
2025-08-12 15:30:20 +0000

    added larx generator

--- cayleypy/graphs_lib_test.py
+++ cayleypy/graphs_lib_test.py
@@ -265,6 +265,11 @@ def test_wrapped_k_cycles():
     graph = PermutationGroups.wrapped_k_cycles(5, 3)
     assert graph.generators == [[1, 2, 0, 3, 4], [0, 2, 3, 1, 4], [0, 1, 3, 4, 2], [3, 1, 2, 4, 0], [1, 4, 2, 3, 0]]
 
+def test_larx():
+    graph1 = PermutationGroups.larx(3)
+    assert graph1.generators == [[1, 0, 2], [0, 2, 1]]
+    graph2 = PermutationGroups.larx(4)
+    assert graph2.generators == [[1, 0, 2, 3], [0, 2, 3, 1]]
 
 def test_heisenberg():
     graph1 = MatrixGroups.heisenberg()
