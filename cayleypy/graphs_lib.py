c31e840754dd45feb844ce21a3e604ceb2907479
Arsenii Sychev <57996364+messoem@users.noreply.github.com>
2025-09-22 16:12:27 +0300

    Update graphs_lib.py

--- cayleypy/graphs_lib.py
+++ cayleypy/graphs_lib.py
@@ -602,7 +602,7 @@ class PermutationGroups:
         6. The final generators are obtained by multiplying all the permutations assigned to them.
         """
         assert 1 <= k <= n - 3, "k must be >= 1 and <= n - 3"
-        
+
         p1 = permutation_from_cycles(n, [[i, i + 1] for i in range(0, n - 1, 2)])
         p2 = permutation_from_cycles(n, [[i, i + 1] for i in range(1, n - 1, 2)])
         if k % 2 == 1:
@@ -648,7 +648,7 @@ class PermutationGroups:
         generator_names = ["A", "S"]
         name = f"sheveleva2-n{n}-k{k}"
         return CayleyGraphDef.create(
-            generators, central_state = list(range(n)), generator_names=generator_names, name=name
+            generators, central_state=list(range(n)), generator_names=generator_names, name=name
         )
 
     @staticmethod
